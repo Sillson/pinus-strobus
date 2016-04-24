714d97d3d70fed9b959de0ffd09a87fe0a16009f
Stuart Illson <sillson88@gmail.com>
2016-04-24 11:33:40 -0700

    finished tutorial

--- ui.R
+++ ui.R
@@ -9,20 +9,17 @@ shinyUI(fluidPage(
     sidebarPanel(
       helpText("Create demographic maps with 
         information from the 2010 US Census."),
-      
+    
       selectInput("var", 
         label = "Choose a variable to display",
         choices = choice_opts,
         selected = choice_opts[1]),
-      
+    
       sliderInput("range", 
         label = "Range of interest:",
         min = 0, max = 100, value = c(0, 100))
     ),
-    
-    mainPanel(
-      textOutput("text1"),
-      textOutput("rangetext")
-    )
+  
+    mainPanel(plotOutput("map"))
   )
 ))
